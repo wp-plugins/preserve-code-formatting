fec3cf985053addcae4b5be8416b8a1ccaa1fe27
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-30 16:23:59 +0000

    Fixed long-running bug that caused some preserved code to appear garbled; updated a lot of internal plugin management code; added 'Settings' link to plugin's plugin listing entry; used plugins_url() instead of hardcoded path; brought admin markup in line with modern conventions; minor reformatting (spacing); noted compatibility through WP2.8+; dropped support for pre-WP2.6; tweaked descriptions; updated copyright date; updated to v2.5
    git-svn-id: https://plugins.svn.wordpress.org/preserve-code-formatting/trunk@140944 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- preserve-code-formatting.php
+++ preserve-code-formatting.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Preserve Code Formatting
-Version: 2.0
+Version: 2.5
 Plugin URI: http://coffee2code.com/wp-plugins/preserve-code-formatting
 Author: Scott Reilly
 Author URI: http://coffee2code.com
@@ -41,7 +41,7 @@ INSERT INTO $tablepostmeta
 VALUES ('$post_id','link','$extended')
 ");
 
-Compatible with WordPress 2.0+, 2,1+, 2.2+, 2.3+, and 2.5.
+Compatible with WordPress 2.6+, 2.7+, 2.8+.
 
 =>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
 =>> for more information and the latest updates
@@ -51,13 +51,14 @@ Installation:
 1. Download the file http://coffee2code.com/wp-plugins/preserve-code-formatting.zip and unzip it into your 
 /wp-content/plugins/ directory.
 2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Go to the Options -> Code Formatting (or in WP 2.5: Settings -> Code Formatting) admin options page.
-Optionally customize the options.
+3. Go to the Settings -> Code Formatting admin settings page (which you can also get to via the Settings
+link next to the plugin on the Manage Plugins page) and customize the settings.
+4. Write a post with code contained within `<code>` and `</code>` tags (using the HTML editor, not the Visual editor).
 
 */
 
 /*
-Copyright (c) 2004-2008 by Scott Reilly (aka coffee2code)
+Copyright (c) 2004-2009 by Scott Reilly (aka coffee2code)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
@@ -80,8 +81,13 @@ class PreserveCodeFormatting {
 	var $show_admin = true;	// Change this to false if you don't want the plugin's admin page shown.
 	var $config = array();
 	var $options = array(); // Don't use this directly
+	var $plugin_basename = '';
+	var $menu_name = '';
 
 	function PreserveCodeFormatting() {
+		$this->plugin_name = __('Preserve Code Formatting');
+		$this->menu_name = __('Code Formatting');
+		$this->plugin_basename = plugin_basename(__FILE__);
 		$this->config = array(
 			// input can be 'checkbox', 'text', 'hidden', or 'none'
 			'preserve_tags' => array('input' => 'text', 'default' => array('code', 'pre'), 'datatype' => 'array',
@@ -116,7 +122,7 @@ class PreserveCodeFormatting {
 		add_filter('excerpt_save_pre', array(&$this, 'preserve_postprocess'), 100);
 
 		// Comment out these next lines if you don't want to allow preserve code formatting for comments.
-		if ($options['preserve_in_comments']) {
+		if ( $options['preserve_in_comments'] ) {
 			add_filter('comment_text', array(&$this, 'preserve_preprocess'), 2);
 			add_filter('comment_text', array(&$this, 'preserve_postprocess_and_preserve'), 100);
 			add_filter('pre_comment_content', array(&$this, 'preserve_preprocess'), 2);
@@ -130,24 +136,32 @@ class PreserveCodeFormatting {
 	}
 
 	function admin_menu() {
-		if ($this->show_admin)
-			add_options_page('Preserve Code Formatting', 'Code Formatting', 9, basename(__FILE__), array(&$this, 'options_page'));
+		if ( $this->show_admin ) {
+			global $wp_version;
+			if ( current_user_can('manage_options') ) {
+				if ( version_compare( $wp_version, '2.6.999', '>' ) )
+					add_filter( 'plugin_action_links_' . $this->plugin_basename, array(&$this, 'plugin_action_links') );
+				add_options_page($this->menu_name, $this->menu_name, 9, $this->plugin_basename, array(&$this, 'options_page'));
+			}
+		}
+	}
+
+	function plugin_action_links( $action_links ) {
+		$settings_link = '<a href="options.php?page='.$this->plugin_basename.'">' . __('Settings') . '</a>';
+		array_unshift( $action_links, $settings_link );
+
+		return $action_links;
 	}
 
 	function get_options() {
-		if (!empty($this->options)) return $this->options;
+		if ( !empty($this->options) ) return $this->options;
 		// Derive options from the config
 		$options = array();
 		foreach (array_keys($this->config) as $opt) {
 			$options[$opt] = $this->config[$opt]['default'];
 		}
-        $existing_options = get_option($this->admin_options_name);
-        if (!empty($existing_options)) {
-            foreach ($existing_options as $key => $value)
-                $options[$key] = $value;
-        }
-		$this->options = $options;
-        return $options;
+		$this->options = wp_parse_args(get_option($this->admin_options_name), $options);
+		return $this->options;
 	}
 
 	function options_page() {
@@ -157,23 +171,40 @@ class PreserveCodeFormatting {
 			check_admin_referer($this->nonce_field);
 
 			foreach (array_keys($options) AS $opt) {
-				$options[$opt] = $_POST[$opt];
-				if (($this->config[$opt]['input'] == 'checkbox') && !$options[$opt])
+				$options[$opt] = htmlspecialchars(stripslashes($_POST[$opt]));
+				$input = $this->config[$opt]['input'];
+				if (($input == 'checkbox') && !$options[$opt])
 					$options[$opt] = 0;
-				if ($this->config[$opt]['datatype'] == 'array')
-					$options[$opt] = explode(',', str_replace(array(', ', ' ', ','), ',', $options[$opt]));
+				if ($this->config[$opt]['datatype'] == 'array') {
+					if ($input == 'text')
+						$options[$opt] = explode(',', str_replace(array(', ', ' ', ','), ',', $options[$opt]));
+					else
+						$options[$opt] = array_map('trim', explode("\n", trim($options[$opt])));
+				}
+				elseif ($this->config[$opt]['datatype'] == 'hash') {
+					if ( !empty($options[$opt]) ) {
+						$new_values = array();
+						foreach (explode("\n", $options[$opt]) AS $line) {
+							list($shortcut, $text) = array_map('trim', explode("=>", $line, 2));
+							if (!empty($shortcut)) $new_values[str_replace('\\', '', $shortcut)] = str_replace('\\', '', $text);
+						}
+						$options[$opt] = $new_values;
+					}
+				}
 			}
 			// Remember to put all the other options into the array or they'll get lost!
 			update_option($this->admin_options_name, $options);
 
-			echo "<div class='updated'><p>Plugin settings saved.</p></div>";
+			echo "<div id='message' class='updated fade'><p><strong>" . __('Settings saved.') . '</strong></p></div>';
 		}
 
-		$action_url = $_SERVER[PHP_SELF] . '?page=' . basename(__FILE__);
+		$action_url = $_SERVER[PHP_SELF] . '?page=' . $this->plugin_basename;
+		$logo = plugins_url() . '/' . basename($_GET['page'], '.php') . '/c2c_minilogo.png';
 
 		echo <<<END
 		<div class='wrap'>
-			<h2>Preserve Code Formatting Plugin Options</h2>
+			<div class="icon32" style="width:44px;"><img src='$logo' alt='A plugin by coffee2code' /><br /></div>
+			<h2>{$this->plugin_name} Settings</h2>
 			<p>Preserve formatting for text within &lt;code> and &lt;pre> tags (other tags can be defined as well).
 			Helps to preserve code indentation, multiple spaces, prevents WP's fancification of text (ie. ensures 
 			quotes don't become curly, etc).</p>
@@ -184,45 +215,110 @@ class PreserveCodeFormatting {
 			<form name="preserve_code_formatting" action="$action_url" method="post">	
 END;
 				wp_nonce_field($this->nonce_field);
-		echo '<fieldset class="option"><table width="100%" cellspacing="2" cellpadding="5" class="optiontable editform">';
+		echo '<table width="100%" cellspacing="2" cellpadding="5" class="optiontable editform form-table"><tbody>';
 				foreach (array_keys($options) as $opt) {
 					$input = $this->config[$opt]['input'];
-					if ($input == 'none') continue;
+					if ( $input == 'none' ) continue;
 					$label = $this->config[$opt]['label'];
 					$value = $options[$opt];
-					if (($input == 'checkbox') && ($value == 1)) {
-						$checked = 'checked=checked ';
+					if ( $input == 'checkbox' ) {
+						$checked = ($value == 1) ? 'checked=checked ' : '';
 						$value = 1;
 					} else {
 						$checked = '';
 					};
-					if ($this->config[$opt]['datatype'] == 'array')
-						$value = implode(', ', $value);
-					echo "<tr valign='top'><th width='50%' scope='row'>$label : </th>";
-					echo "<td><input name='$opt' type='$input' id='$opt' value='$value' $checked/>";
-					if ($this->config[$opt]['help']) {
+					if ( $this->config[$opt]['datatype'] == 'array' ) {
+						if ( !is_array($value) )
+							$value = '';
+						else {
+							if ( $input == 'textarea' || $input == 'inline_textarea' )
+								$value = implode("\n", $value);
+							else
+								$value = implode(', ', $value);
+						}
+					} elseif ( $this->config[$opt]['datatype'] == 'hash' ) {
+						if ( !is_array($value) )
+							$value = '';
+						else {
+							$new_value = '';
+							foreach ($value AS $shortcut => $replacement) {
+								$new_value .= "$shortcut => $replacement\n";
+							}
+							$value = $new_value;
+						}
+					}
+					echo "<tr valign='top'>";
+					if ( $input == 'textarea' ) {
+						echo "<td colspan='2'>";
+						if ( $label ) echo "<strong>$label</strong><br />";
+						echo "<textarea name='$opt' id='$opt' {$this->config[$opt]['input_attributes']}>" . $value . '</textarea>';
+					} else {
+						echo "<th scope='row'>$label</th><td>";
+						if ( $input == "inline_textarea" )
+							echo "<textarea name='$opt' id='$opt' {$this->config[$opt]['input_attributes']}>" . $value . '</textarea>';
+						elseif ( $input == 'select' ) {
+							echo "<select name='$opt' id='$opt'>";
+							foreach ($this->config[$opt]['options'] as $sopt) {
+								$selected = $value == $sopt ? " selected='selected'" : '';
+								echo "<option value='$sopt'$selected>$sopt</option>";
+							}
+							echo "</select>";
+						} else {
+							$tclass = ($input == 'short_text') ? 'small-text' : 'regular-text';
+							if ($input == 'short_text') $input = 'text';
+							echo "<input name='$opt' type='$input' id='$opt' value='$value' class='$tclass' $checked {$this->config[$opt]['input_attributes']} />";
+						}
+					}
+					if ( $this->config[$opt]['help'] ) {
 						echo "<br /><span style='color:#777; font-size:x-small;'>";
 						echo $this->config[$opt]['help'];
 						echo "</span>";
 					}
 					echo "</td></tr>";
 				}
+		$save_text = __('Save Changes');
 		echo <<<END
-			</table>
-			</fieldset>
+			</tbody></table>
 			<input type="hidden" name="submitted" value="1" />
-			<div class="submit"><input type="submit" name="Submit" value="Update Options &raquo;" /></div>
+			<div class="submit"><input type="submit" name="Submit" class="button-primary" value="{$save_text}" /></div>
 		</form>
 			</div>
 END;
 		echo <<<END
-		<div class='wrap' style="text-align:center; color:#888;">This plugin brought to you by <a href="http://coffee2code.com" title="coffee2code.com">Scott Reilly, aka coffee2code</a>.<br /><span style="font-size:x-small;"><a href="http://coffee2code.com/donate">Did you find this plugin useful?</a></span></div>
+		<style type="text/css">
+			#c2c {
+				text-align:center;
+				color:#888;
+				background-color:#ffffef;
+				padding:5px 0 0;
+				margin-top:12px;
+				border-style:solid;
+				border-color:#dadada;
+				border-width:1px 0;
+			}
+			#c2c div {
+				margin:0 auto;
+				padding:5px 40px 0 0;
+				width:45%;
+				min-height:40px;
+				background:url('$logo') no-repeat top right;
+			}
+			#c2c span {
+				display:block;
+				font-size:x-small;
+			}
+		</style>
+		<div id='c2c' class='wrap'>
+			<div>
+			This plugin brought to you by <a href="http://coffee2code.com" title="coffee2code.com">Scott Reilly, aka coffee2code</a>.
+			<span><a href="http://coffee2code.com/donate" title="Please consider a donation">Did you find this plugin useful?</a></span>
+			</div>
+		</div>
 END;
 	}
 
 	function prep_code( $text ) {
 		$options = $this->get_options();
-
 		$text = preg_replace("/(\r\n|\n|\r)/", "\n", $text);
 		$text = preg_replace("/\n\n+/", "\n\n", $text);
 		$text = str_replace(array("&#36&;", "&#39&;"), array("$", "'"), $text);
@@ -244,15 +340,15 @@ END;
 		$options = $this->get_options();
 		$preserve_tags = $options['preserve_tags'];
 		$result = '';
-		foreach ($preserve_tags as $tag) {
-			if (!empty($result)) {
+		foreach ( $preserve_tags as $tag ) {
+			if ( !empty($result) ) {
 				$content = $result;
 				$result = '';
 			}
 			$codes = preg_split("/(<{$tag}[^>]*>.*<\\/{$tag}>)/Us", $content, -1, PREG_SPLIT_DELIM_CAPTURE);
-			foreach ($codes as $code) {
-				if (preg_match("/^<{$tag}[^>]*>(.*)<\\/{$tag}>/Us", $code, $match))
-					$code = "[[{$tag}]]" . base64_encode(stripslashes($match[1])) . "[[/{$tag}]]";
+			foreach ( $codes as $code ) {
+				if ( preg_match("/^<{$tag}[^>]*>(.*)<\\/{$tag}>/Us", $code, $match) )
+					$code = "[[{$tag}]]" . base64_encode(addslashes($match[1]))  . "[[/{$tag}]]";
 				$result .= $code;
 			}
 		}
@@ -265,16 +361,16 @@ END;
 		$preserve_tags = $options['preserve_tags'];
 		$wrap_multiline_code_in_pre = $options['wrap_multiline_code_in_pre'];
 		$result = '';
-		foreach ($preserve_tags as $tag) {
-			if (!empty($result)) {
+		foreach ( $preserve_tags as $tag ) {
+			if ( !empty($result) ) {
 				$content = $result;
 				$result = '';
 			}
 			$codes = preg_split("/(\\[\\[{$tag}\\]\\].*\\[\\[\\/{$tag}\\]\\])/Us", $content, -1, PREG_SPLIT_DELIM_CAPTURE);
-			foreach ($codes as $code) {
-				if (preg_match("/\\[\\[{$tag}\\]\\](.*)\\[\\[\\/{$tag}\\]\\]/Us", $code, $match)) {
+			foreach ( $codes as $code ) {
+				if ( preg_match("/\\[\\[{$tag}\\]\\](.*)\\[\\[\\/{$tag}\\]\\]/Us", $code, $match) ) {
 					$data = base64_decode($match[1]);
-					if ($preserve) $data = $this->preserve_code_formatting($data);
+					if ( $preserve ) $data = $this->preserve_code_formatting($data);
 					else $data = $wpdb->escape($data);
 					$code = "<$tag>$data</$tag>";
 					if ( $preserve && $wrap_multiline_code_in_pre && preg_match("/\n/", $data) )
@@ -293,13 +389,11 @@ END;
 } // end PreserveCodeFormatting
 
 endif; // end if !class_exists()
+
 if ( class_exists('PreserveCodeFormatting') ) :
-	// Get the ball rolling
 	$preserve_code_formatting = new PreserveCodeFormatting();
-	// Actions and filters
-	if (isset($preserve_code_formatting)) {
+	if ( isset($preserve_code_formatting) )
 		register_activation_hook( __FILE__, array(&$preserve_code_formatting, 'install') );
-	}
 endif;
 
 ?>
